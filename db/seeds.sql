44e2b048aba478c25bfa728a96213997e6b4da98
mianak78 <rosebourn@gmail.com>
2020-01-22 12:31:36 -0700

    needs styling and button fixes

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Swiss Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Jalapeno Burger");
