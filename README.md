c1a5e8ff3444b152a44d3b43a941c5868dd6b321
mianak78 <rosebourn@gmail.com>
2020-07-24 18:35:06 -0600

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Burger
 
-A burger logger with MySQL, Node, Express, Handlebars and an ORM.
+A burger logger to keep track of all the burgers to be devoured! When the burger has been eaten, click the button to move burger from the "Burgers to Eat" column to the "Devoured" column.
+
+## Test it out [here](https://fathomless-tundra-95725.herokuapp.com/)
+
+Add the burger name to input box at the bottom of the page and then click the "Submit" button. It will then be added to your "Burgers to Eat" list. Once you have enjoyed the burger, click the "Eat-Da-Burger" button to move it to the "Devoured" list.
+
+## Technologies
+
+- Handlebars
+- Node
+- Express
+- MySQL
+- CSS
+- ORM
+- Heroku
+
+![](images/screenShot.png)
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,37 +1,35 @@
 $(function() {
-    $(".change-to-eaten").on("click", function(event) {
-      var id = $(this).data("id");
-      var newEaten = $(this).data("neweaten");
-  
-      var newEatenAdd = {
-        devoured: newEaten
-      };
-  
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newEatenAdd
-      }).then(
-        function() {
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#br").val().trim(),
-      };
-  
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          location.reload();
-        }
-      );
+  $(".change-to-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("neweaten");
+
+    var newEatenAdd = {
+      devoured: newEaten
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenAdd
+    }).then(function() {
+      location.reload();
     });
   });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#br")
+        .val()
+        .trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new burger");
+      location.reload();
+    });
+  });
+});
